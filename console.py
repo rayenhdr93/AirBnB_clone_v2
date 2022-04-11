8975fa723ed5e8d760e8955ebf618e1f5d86594e
cryptolake <dhiadah@gmail.com>
2022-04-11 23:18:40 +0100

    create instance with key values.

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -113,15 +113,49 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
+    @staticmethod
+    def check_type(value):
+        """ check if string."""
+        arg = ""
+        if value[0] == '"' and value[-1] == '"':
+            for i in range(1, len(value)-1):
+                if value[i] == '"' and value[i-1] != '\\':
+                    return None
+                if value[i] == "_":
+                    arg += " "
+                else:
+                    arg += value[i]
+            return arg
+        elif '.' in value and float(value):
+            return float(value)
+        elif int(value):
+            return int(value)
+        else:
+            return None
+            
     def do_create(self, args):
         """ Create an object of any class"""
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        args = args.split()
+        if args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        new_instance = HBNBCommand.classes[args[0]]()
+        args.pop(0)
+        for arg in args:
+            arg = arg.split('=')
+            if len(arg) != 2:
+                continue
+            key = arg[0]
+            value = arg[1]
+            value = self.check_type(value)
+            if value is None:
+                continue
+            else:
+                setattr(new_instance, key, value)
+
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -272,7 +306,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +314,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
