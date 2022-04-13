24c18cc58788c9d8cb865ade3f70b46c99bd9d52
cryptolake <dhiadah@gmail.com>
2022-04-13 22:33:11 +0100

    Revert "testing full storage switching"
    This reverts commit 352515ea3193b945a5db3b64f4ff00d74e68b2a3.

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User."""
 
-from models import storage_t
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -10,20 +9,10 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """Define a user by various attributes."""
 
-    if storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize Model."""
-        super().__init__(*args, **kwargs)
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user")
+    reviews = relationship("Review", backref="user")
