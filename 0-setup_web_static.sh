01ab13bb0a929cf074f00178eee9fc6d48670487
cryptolake <dhiadah@gmail.com>
2022-04-28 16:55:52 +0100

    lots of fixin

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 # setup web static
-mkdir -p /data/web_static/releases/ 2>/dev/null
-mkdir -p /data/web_static/shared/ 2>/dev/null
-mkdir -p /data/web_static/releases/test/ 2>/dev/null
+mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/ 2>/dev/null
 echo "<h1>wow this is a test</h1>" > /data/web_static/releases/test/index.html
-ln -s /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
 config="
 server {
 	listen 80 default_server;
-	listen [::]:80 default_server;
+
+	server_name _;
+
+	location / {
+		try_files \$uri \$uri/ =404;
+	}
+
 
 	location /hbnb_static {
 		alias /data/web_static/current;
+		index index.html;
+		try_files \$uri \$uri /hbnb_static/index.html;
 	}
 }
 "
 echo -e "$config" > /etc/nginx/sites-available/default
-systemctl restart nginx
+
+service nginx restart
