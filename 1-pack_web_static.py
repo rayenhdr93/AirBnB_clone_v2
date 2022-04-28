168ed0ba21400c07486e9a9faa9934499723fa29
cryptolake <dhiadah@gmail.com>
2022-04-28 18:33:16 +0100

    packing

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """Compress static files."""
-from time import localtime
+from datetime import datetime
 from fabric import task
 
 
 @task
 def do_pack(c):
     """Compress and version."""
-    c.run('mkdir versions')
-    tm = localtime()
-    path = f"./versions/web_static_{tm.tm_year}{tm.tm_mon}\
-            {tm.tm_mday}{tm.tm_hour}{tm.tm_min}{tm.tm_sec}.tgz"
-    c.run(f"tar -zcvf {path} ./web_static")
+    try:
+        c.run('mkdir versions')
+        tm = datetime.now().strftime('%Y%m%d%H%M%s')
+        path = "./versions/web_static_{}.tgz".format(tm)
+        c.run("tar -zcvf {} ./web_static".format(path))
+    except:
+        return None
     return path
