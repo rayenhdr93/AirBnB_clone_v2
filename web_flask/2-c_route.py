0975153c2bfc0e8dc3e842a99e5503adc1a5c98b
rayenhdr93 <hedrirayen69@gmail.com>
2022-05-14 15:44:40 +0100

    2-

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''script that starts a Flask web application'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    return 'C %s' % text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
