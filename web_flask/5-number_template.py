37d1d5089c963c7adef1dc748032938c4d6fc2b3
rayenhdr93 <hedrirayen69@gmail.com>
2022-05-15 19:51:31 +0100

    5-py 5-html

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''script that starts a Flask web application'''
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    text = text.replace("_", " ")
+    return 'C %s' % text
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pyth(text='is cool'):
+    text = text.replace("_", " ")
+    return 'Python %s' % text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '%s is a number' % n
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
